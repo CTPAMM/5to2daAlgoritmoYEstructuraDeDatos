c18f555d6aaf78cba9e8d7e2c4c24114c658eb14
Pablo <gpabloandres@gmail.com>
2023-06-26 13:06:14 -0300

    Actualiza repo

---
+++ Trabajos Prácticos/tpesPrácticaPython/tppyhton lautaro subiabre 5 2.py
@@ -0,0 +1,23 @@
+print("TP Evaluativo de Phyton")
+print("1- ¡Que es python?")
+print("2- ¿Que se puede programar con python?")
+print("3- imprimir ub calculo matematico en python")
+
+print("desarrollo del cuestionario:")
+print("1: Python es un lenguaje de alto nivel de programación interpretado cuya filosofía hace hincapié en la legibilidad de su código, se utiliza para desarrollar aplicaciones de todo tipo, ejemplos: Instagram, Netflix, Spotify, Panda3D, entre otros.")
+print("2:Desarrollo de aplicaciones web con frameworks como Django o Flask. Automatización de tareas y procesamiento de datos.")
+print("Machine learning y deep learning con librerías como TensorFlow y PyTorch. Desarrollo de juegos con librerías como Pygame")
+print("3: desarrollo del calculo:")
+ 
+ #defino la funcion sumar
+ 
+def sumar(a, b):
+     resultado = a + b 
+     return resultado 
+ 
+ #implemento la funcion
+numero1 = int(input("ingrese el primer numero:"))
+numero2 = int(input("ingrese el primer numero:"))
+ 
+print(f"la suma de ambos numeros es{sumar(numero1, numero2)}")
+
