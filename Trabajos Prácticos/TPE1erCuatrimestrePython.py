c15b89d4c686773543963563eaa632bb228a6a6b
Pablo <gpabloandres@gmail.com>
2023-07-10 09:33:37 -0300

    Actualiza repo

--- Trabajos Prácticos/TPE1erCuatrimestrePython.py
+++ Trabajos Prácticos/TPE1erCuatrimestrePython.py
@@ -1,31 +1,25 @@
 # -------------------------------------------------------------------------------
-# Fecha: 28-06-23
-# Curso: 
-# Estudiante: 
+# Fecha: 05-07-23
+# Curso: 4° 2°
+# Estudiante: Su nombre completo 
 # Título: Trabajo Práctico Evaluativo del 1er Cuatrimestre
 #--------------------------------------------------------------------------------
-
 '''
 1) Escribe un programa muestre por pantalla “Hello World”.
 
 2) Escribe un programa que escriba en la pantalla el resultado de sumar 3 + 5.
 
-3) Escribe un programa que pida un número, pida otro número y escriba el resultado de multiplicar estos dos números.
+3) Escribe un programa que pida el primer nombre, segundo nombre y apellido y escriba un texto que diga “Hola nombre completo”
 
-4) Rescribe el programa 3 pero que implemente una función.
+4) Escribe un programa que pida un número, pida otro número y escriba el resultado de multiplicar estos dos números.
 
-5) Escribe un programa que mediante una función pida el nombre del usuario y escriba un texto que diga “Hola nombreUsuario”
+5) Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
 
-6) Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.(Incluye excepciones)
+6) Escribe un programa que pida un número y diga si es divisible por 2
 
-7) Rescribe el programa 6 pero que implemente una función.
+7) Escribe un programa que pida un número y nos diga si es divisible por 2 y 3 
 
-8) Escribe un programa que implemente una función que pida un número y diga si es divisible por 2
-
-9) Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7.(sólo hay que comprobar si lo es por uno de los cuatro)
-
-10) Escribe un programa que implemente una función que pida una nota (número) y muestre la calificación según la nota:
-
+8) Pide una nota (número). Muestra la calificación según la nota:
  0-3: Muy deficiente
  3-5: Insuficiente
  5-6: Suficiente
@@ -33,25 +27,19 @@
  7-9: Notable
  9-10: Sobresaliente
 
-11) Crea una lista llamada "lista_numeros" y almacena los siguientes valores integer:
-        
-                    10,45,356,50,10,15,46,67,45,10,28,43,10,65,10,33
+9) Crea una lista llamada "lista_numeros" y almacena los siguientes valores integer:
 
-    De la lista anterior:
+10,45,356,50,10,15,46,67,45,10,28,43,10,65,10,33
+   a) Imprime el valor menor y mayor.
+   b) Imprime la lista ordenada de menor a mayor
+   c) Imprime el largo de la lista
+   d) Imprime cuantas veces se repite el número 10
 
-        a) Imprime el valor menor y mayor.
-        b) Imprime la lista ordenada de menor a mayor
-        c) Imprime el largo de la lista
-        d) Imprime cuantas veces se repite el número 10
+10) Crea una lista llamada "lista_colores" y almacena los siguientes valores string:
+rojo, azul, verde, amarillo
 
-12) Crea una lista llamada "lista_colores" y almacena los siguientes valores string:
-
-                    rojo, azul, verde, amarillo
-    De la lista anterior:
-
-        a) Imprime la lista modificada luego de añadir los colores en el código en este orden:
-        
-                gris - Antes de "rojo".
-                rosa - En último lugar.
-                naranja - Entre "azul" y "verde".
+    a) Imprime la lista modificada luego de añadir los colores en el código en este orden:
+        gris - Antes de "rojo".
+        rosa - En último lugar.
+        naranja - Entre "azul" y "verde".
 '''
